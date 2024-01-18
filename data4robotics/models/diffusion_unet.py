8b24535479cdda919d819460562efacdfc67d236
Joey Hejna <joey.hejna@gmail.com>
2024-01-18 12:44:47 -0800

    add unet diffusion

---
+++ data4robotics/models/diffusion_unet.py
@@ -0,0 +1,353 @@
+# Copyright (c) Sudeep Dasari, 2023
+
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+import torch
+import numpy as np
+import torch.nn as nn
+from data4robotics.agent import Agent
+
+from typing import Union, Optional
+import torch
+import torch.nn as nn
+import math
+
+from diffusers.schedulers.scheduling_ddim import DDIMScheduler
+
+
+class SinusoidalPosEmb(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        device = x.device
+        half_dim = self.dim // 2
+        emb = math.log(10000) / (half_dim - 1)
+        emb = torch.exp(torch.arange(half_dim, device=device) * -emb)
+        emb = x[:, None] * emb[None, :]
+        emb = torch.cat((emb.sin(), emb.cos()), dim=-1)
+        return emb
+
+
+class Downsample1d(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.conv = nn.Conv1d(dim, dim, 3, 2, 1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class Upsample1d(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.conv = nn.ConvTranspose1d(dim, dim, 4, 2, 1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+class Conv1dBlock(nn.Module):
+    """
+    Conv1d --> GroupNorm --> Mish
+    """
+
+    def __init__(self, inp_channels, out_channels, kernel_size, n_groups=8):
+        super().__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv1d(inp_channels, out_channels, kernel_size, padding=kernel_size // 2),
+            nn.GroupNorm(n_groups, out_channels),
+            nn.Mish(),
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class ConditionalResidualBlock1D(nn.Module):
+    def __init__(self, in_channels, out_channels, cond_dim, kernel_size=3, n_groups=8):
+        super().__init__()
+
+        self.blocks = nn.ModuleList(
+            [
+                Conv1dBlock(in_channels, out_channels, kernel_size, n_groups=n_groups),
+                Conv1dBlock(out_channels, out_channels, kernel_size, n_groups=n_groups),
+            ]
+        )
+
+        # FiLM modulation https://arxiv.org/abs/1709.07871
+        # predicts per-channel scale and bias
+        cond_channels = out_channels * 2
+        self.out_channels = out_channels
+        self.cond_encoder = nn.Sequential(
+            nn.Mish(), nn.Linear(cond_dim, cond_channels), nn.Unflatten(-1, (-1, 1))
+        )
+
+        # make sure dimensions compatible
+        self.residual_conv = (
+            nn.Conv1d(in_channels, out_channels, 1)
+            if in_channels != out_channels
+            else nn.Identity()
+        )
+
+    def forward(self, x, cond):
+        """
+        x : [ batch_size x in_channels x horizon ]
+        cond : [ batch_size x cond_dim]
+
+        returns:
+        out : [ batch_size x out_channels x horizon ]
+        """
+        out = self.blocks[0](x)
+        embed = self.cond_encoder(cond)
+
+        embed = embed.reshape(embed.shape[0], 2, self.out_channels, 1)
+        scale = embed[:, 0, ...]
+        bias = embed[:, 1, ...]
+        out = scale * out + bias
+
+        out = self.blocks[1](out)
+        out = out + self.residual_conv(x)
+        return out
+
+
+class ConditionalUnet1D(nn.Module):
+    def __init__(
+        self,
+        input_dim,
+        global_cond_dim,
+        diffusion_step_embed_dim=256,
+        down_dims=[256, 512, 1024],
+        kernel_size=3,
+        n_groups=8,
+    ):
+        """
+        input_dim: Dim of actions.
+        global_cond_dim: Dim of global conditioning applied with FiLM
+            in addition to diffusion step embedding. This is usually obs_horizon * obs_dim
+        diffusion_step_embed_dim: Size of positional encoding for diffusion iteration k
+        down_dims: Channel size for each UNet level.
+            The length of this array determines numebr of levels.
+        kernel_size: Conv kernel size
+        n_groups: Number of groups for GroupNorm
+        """
+        super().__init__()
+        all_dims = [input_dim] + list(down_dims)
+        start_dim = down_dims[0]
+
+        dsed = diffusion_step_embed_dim
+        diffusion_step_encoder = nn.Sequential(
+            SinusoidalPosEmb(dsed),
+            nn.Linear(dsed, dsed * 4),
+            nn.Mish(),
+            nn.Linear(dsed * 4, dsed),
+        )
+        cond_dim = dsed + global_cond_dim
+
+        in_out = list(zip(all_dims[:-1], all_dims[1:]))
+        mid_dim = all_dims[-1]
+        self.mid_modules = nn.ModuleList(
+            [
+                ConditionalResidualBlock1D(
+                    mid_dim, mid_dim, cond_dim=cond_dim, kernel_size=kernel_size, n_groups=n_groups
+                ),
+                ConditionalResidualBlock1D(
+                    mid_dim, mid_dim, cond_dim=cond_dim, kernel_size=kernel_size, n_groups=n_groups
+                ),
+            ]
+        )
+
+        down_modules = nn.ModuleList([])
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (len(in_out) - 1)
+            down_modules.append(
+                nn.ModuleList(
+                    [
+                        ConditionalResidualBlock1D(
+                            dim_in,
+                            dim_out,
+                            cond_dim=cond_dim,
+                            kernel_size=kernel_size,
+                            n_groups=n_groups,
+                        ),
+                        ConditionalResidualBlock1D(
+                            dim_out,
+                            dim_out,
+                            cond_dim=cond_dim,
+                            kernel_size=kernel_size,
+                            n_groups=n_groups,
+                        ),
+                        Downsample1d(dim_out) if not is_last else nn.Identity(),
+                    ]
+                )
+            )
+
+        up_modules = nn.ModuleList([])
+        for ind, (dim_in, dim_out) in enumerate(reversed(in_out[1:])):
+            is_last = ind >= (len(in_out) - 1)
+            up_modules.append(
+                nn.ModuleList(
+                    [
+                        ConditionalResidualBlock1D(
+                            dim_out * 2,
+                            dim_in,
+                            cond_dim=cond_dim,
+                            kernel_size=kernel_size,
+                            n_groups=n_groups,
+                        ),
+                        ConditionalResidualBlock1D(
+                            dim_in,
+                            dim_in,
+                            cond_dim=cond_dim,
+                            kernel_size=kernel_size,
+                            n_groups=n_groups,
+                        ),
+                        Upsample1d(dim_in) if not is_last else nn.Identity(),
+                    ]
+                )
+            )
+
+        final_conv = nn.Sequential(
+            Conv1dBlock(start_dim, start_dim, kernel_size=kernel_size),
+            nn.Conv1d(start_dim, input_dim, 1),
+        )
+
+        self.diffusion_step_encoder = diffusion_step_encoder
+        self.up_modules = up_modules
+        self.down_modules = down_modules
+        self.final_conv = final_conv
+
+        print("number of parameters: {:e}".format(sum(p.numel() for p in self.parameters())))
+
+    def forward(
+        self,
+        sample: torch.Tensor,
+        timestep: Union[torch.Tensor, int],
+        global_cond: Optional[torch.Tensor] = None,
+    ):
+        """
+        x: (B,T,input_dim)
+        timestep: (B,) or int, diffusion step
+        global_cond: (B,global_cond_dim)
+        output: (B,T,input_dim)
+        """
+        # (B,T,C)
+        sample = sample.moveaxis(-1, -2)
+        # (B,C,T)
+
+        # 1. time
+        timesteps = timestep
+        if not torch.is_tensor(timesteps):
+            timesteps = torch.tensor([timesteps], dtype=torch.long, device=sample.device)
+        elif isinstance(timesteps, torch.Tensor) and len(timesteps.shape) == 0:
+            timesteps = timesteps[None].to(sample.device)
+
+        assert isinstance(timesteps, torch.Tensor)
+        # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
+        timesteps = timesteps.expand(sample.shape[0])
+
+        global_feature: torch.Tensor = self.diffusion_step_encoder(timesteps)
+
+        if global_cond is not None:
+            global_feature = torch.cat([global_feature, global_cond], dim=-1)
+
+        x = sample
+        h = []
+        for idx, (resnet, resnet2, downsample) in enumerate(self.down_modules):
+            x = resnet(x, global_feature)
+            x = resnet2(x, global_feature)
+            h.append(x)
+            x = downsample(x)
+
+        for mid_module in self.mid_modules:
+            x = mid_module(x, global_feature)
+
+        for idx, (resnet, resnet2, upsample) in enumerate(self.up_modules):
+            x = torch.cat((x, h.pop()), dim=1)
+            x = resnet(x, global_feature)
+            x = resnet2(x, global_feature)
+            x = upsample(x)
+
+        x = self.final_conv(x)
+
+        # (B,C,T)
+        x = x.moveaxis(-1, -2)
+        # (B,T,C)
+        return x
+
+
+
+class DiffusionUnetAgent(Agent):
+    def __init__(self, features, shared_mlp, odim, n_cams, use_obs, 
+                 ac_dim, ac_chunk, diffusion_steps, dropout=0,
+                 noise_net_kwargs=dict()):
+        super().__init__(features, None, shared_mlp, odim, n_cams, 
+                         use_obs, dropout)
+
+        self.noise_net = ConditionalUnet1D(input_dim=ac_dim, 
+                                           global_cond_dim=self.obs_enc_dim,
+                                           **noise_net_kwargs)
+        
+        self._ac_dim, self._ac_chunk = ac_dim, ac_chunk
+        self._diffusion_steps = diffusion_steps
+        self.diffusion_schedule = DDIMScheduler(
+                                    num_train_timesteps=100,
+                                    beta_start=0.0001,
+                                    beta_end=0.02,
+                                    beta_schedule="squaredcos_cap_v2",
+                                    clip_sample=True,
+                                    set_alpha_to_one=True,
+                                    steps_offset=0,
+                                    prediction_type="epsilon"
+                                    )
+        
+    def forward(self, imgs, obs, actions):
+        # get observation encoding and sample noise/timesteps
+        B, device = imgs.shape[0], imgs.device
+        s_t = self._shared_forward(imgs, obs)
+        noise = torch.randn_like(actions)
+        timesteps = torch.randint(low=0, high=self._diffusion_steps, size=(B,), 
+                                  device=device).long()
+
+        # construct noise actions given real actions, noise, and diffusion schedule
+        noise_acs = self.diffusion_schedule.add_noise(actions, noise, timesteps)
+        noise_pred = self.noise_net(s_t, noise_acs, timesteps)
+        
+        # calculate loss for noise net
+        loss = nn.functional.mse_loss(noise_pred, noise, reduction="none").sum(dim=1)
+        loss = loss.mean()
+        return loss
+
+    def get_actions(self, imgs, obs):
+        # get observation encoding and sample noise
+        B, device = imgs.shape[0], imgs.device
+        s_t = self._shared_forward(imgs, obs)
+        noise_actions = torch.randn(B, self.ac_chunk, self.ac_chunk, device=device)
+
+        # begin diffusion process
+        self.diffusion_schedule.set_timesteps(self._diffusion_steps)
+        self.diffusion_schedule.alphas_cumprod = self.diffusion_schedule.alphas_cumprod.to(device)
+        for timestep in self.diffusion_schedule.timesteps:
+            # predict noise given timestep
+            time = timestep.unsqueeze(0).repeat(B).to(device)
+            noise_pred = self.noise_net(s_t, noise_actions, time)
+
+            # take diffusion step
+            noise_actions = self.diffusion_schedule.step(model_output=noise_pred, 
+                                                timestep=timestep, 
+                                                sample=noise_actions).prev_sample
+
+        # return final action post diffusion
+        return noise_actions
+    
+    @property
+    def ac_chunk(self):
+        return self._ac_chunk
+
+    @property
+    def ac_dim(self):
+        return self._ac_dim
