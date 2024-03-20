018416922deae2b4d90e1935ec8c1d9d43fd50de
Sudeep Dasari <sdasari@andrew.cmu.edu>
2024-03-20 15:53:16 +0000

    initial attempt at a diffusion transformer

--- data4robotics/models/diffusion.py
+++ data4robotics/models/diffusion.py
@@ -11,6 +11,33 @@ from data4robotics.agent import Agent
 from diffusers.schedulers.scheduling_ddim import DDIMScheduler
 
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=5000, dropout=0.1):
+        super().__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        
+        # Compute the positional encodings once in log space
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2, dtype=torch.float) * \
+                             -(np.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        """
+        Args:
+            x: Tensor of shape (seq_len, batch_size, d_model)
+            
+        Returns:
+            Tensor of shape (seq_len, batch_size, d_model) with positional encodings added
+        """
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
+
+
 class FourierFeatures(nn.Module):
     def __init__(self, time_dim, learnable=False):
         assert time_dim % 2 == 0, "time_dim must be even!"
@@ -27,79 +54,80 @@ class FourierFeatures(nn.Module):
         return torch.cat((torch.cos(x), torch.sin(x)), dim=1)
 
 
-class MLPResNetBlock(nn.Module):
-    def __init__(self, dim, dropout=0.1, layer_norm=True, alpha=2):
+class TransformerBlock(nn.Module):
+    def __init__(self, dim, cond_dim, dropout=0.1, layer_norm=True, num_heads=8):
         super().__init__()
-
-        layers = [nn.Dropout(dropout)]
-        if layer_norm:
-            layers.append(nn.LayerNorm(dim))
+        self.cond_proj = nn.Sequential(nn.Linear(cond_dim, dim),
+                                       nn.ReLU(inplace=True),
+                                       nn.Linear(dim, dim))
+        self.map = nn.MultiheadAttention(dim, num_heads, dropout=dropout)
+        self.norm_map = nn.LayerNorm(dim) if layer_norm \
+                        else nn.Identity()
         
-        layers.append(nn.Linear(dim, alpha * dim))
-        layers.append(nn.ReLU())
-        layers.append(nn.Linear(alpha * dim, dim))
-        layers.append(nn.ReLU())
-        self.net = nn.Sequential(*layers)
+        self.proj_out = nn.Sequential(nn.Linear(dim, dim), 
+                                      nn.ReLU(inplace=True))
+        self.norm_out = nn.LayerNorm(dim) if layer_norm \
+                        else nn.Identity()
 
-    def forward(self, x):
-        return x + self.net(x)
+    def forward(self, ac_tokens, cond_token):
+        # keep re-adding the transform's global conditioning token
+        cond_token = self.cond_proj(cond_token)
 
-
-class MLPResFILMBlock(nn.Module):
-    def __init__(self, dim, cond_dim, dropout=0.1, layer_norm=True, alpha=2):
-        super().__init__()
-
-        top_layers = [nn.Dropout(dropout)]
-        if layer_norm:
-            top_layers.append(nn.LayerNorm(dim))
-        
-        top_layers.append(nn.Linear(dim, alpha * dim))
-        top_layers.append(nn.ReLU())
-        self.top_layers = nn.Sequential(*top_layers)
-
-        self.film_embed = nn.Sequential(nn.Linear(cond_dim, alpha * dim),
-                                        nn.ReLU())
-        self.out_layers = nn.Sequential(nn.Linear(alpha * dim, dim),
-                                        nn.ReLU())                               
-
-    def forward(self, x, cond):
-        out = self.top_layers(x)
-        out = self.film_embed(cond) + out
-        out = self.out_layers(out)
-        return x + out
+        tokens = torch.cat((cond_token[None], ac_tokens), 0)
+        tokens, _ = self.map(tokens, tokens, tokens)
+        tokens = self.norm_map(tokens[1:] + ac_tokens)
+        return self.norm_out(self.proj_out(tokens) + tokens)
 
 
 class NoiseNetwork(nn.Module):
-    def __init__(self, adim, ac_chunk, cond_dim, time_dim=32, learnable_features=True,
-                  num_blocks=3, hidden_dim=256, dropout=0.1, 
-                  use_layer_norm=True):
+    def __init__(self, ac_dim, obs_dim, ac_chunk, time_dim=256, hidden_dim=512,
+                  num_blocks=3, dropout=0.1, learnable_features=True):
         super().__init__()
 
+        # input encoder mlps
+        cond_dim = obs_dim + time_dim
         self.time_net = nn.Sequential(FourierFeatures(time_dim, learnable_features),
                                       nn.Linear(time_dim, time_dim), nn.ReLU())
-        in_dim = adim * ac_chunk + cond_dim + time_dim
-        self.proj = nn.Sequential(nn.Linear(in_dim, hidden_dim), nn.ReLU())
-        net = [MLPResFILMBlock(hidden_dim, in_dim, dropout, use_layer_norm)
-                                                      for _ in range(num_blocks)]
-        self.blocks = nn.ModuleList(net)
-        self.out = nn.Linear(hidden_dim, adim * ac_chunk)
+        self.pos_enc = PositionalEncoding(hidden_dim, dropout=dropout)
+        self.ac_proj = nn.Sequential(nn.Linear(ac_dim, ac_dim),
+                                     nn.ReLU(inplace=True),
+                                     nn.Linear(ac_dim, hidden_dim))
+
+        # transformer blocks
+        self.blocks = nn.ModuleList([TransformerBlock(hidden_dim, cond_dim, dropout) 
+                                                      for _ in range(num_blocks)])        
+        
+        # epsilon pred token (to prompt the transformer)
+        eps_pred_tokens = torch.empty(ac_chunk, 1, hidden_dim)
+        nn.init.xavier_uniform_(eps_pred_tokens)
+        eps_pred_tokens = nn.Parameter(eps_pred_tokens, requires_grad=True)
+        self.register_parameter('eps_pred_tokens', nn.Parameter(eps_pred_tokens))
+
+        # turns predicted tokens into epsilons
+        self.eps_out = nn.Linear(hidden_dim, ac_dim)
 
         print("number of diffusion parameters: {:e}".format(sum(p.numel() for p in self.parameters())))
     
-    def forward(self, noise_ac_flat, time, obs_enc):
+    def forward(self, noise_actions, time, obs_enc):
+        B, T = noise_actions.shape[:2]
         time_enc = self.time_net(time)
-        cond = torch.cat((noise_ac_flat, obs_enc, time_enc), -1)
+        cond_token = torch.cat((obs_enc, time_enc), -1)
         
-        # apply diffusion mlp blocks
-        x = self.proj(cond)
-        for b in self.blocks:
-            x = b(x, cond)
-        
-        # apply final linear layer
-        return self.out(x)
+        ac_tokens = self.ac_proj(noise_actions)
+        ac_tokens = ac_tokens.transpose(0, 1)
+        ac_tokens = torch.cat((ac_tokens, self.eps_pred_tokens.repeat((1, B, 1))), 0)
+        ac_tokens = self.pos_enc(ac_tokens)
+
+        # apply transformer blocks and transpose back
+        for block in self.blocks:
+            ac_tokens = block(ac_tokens, cond_token)
+        ac_tokens = ac_tokens[-T:].transpose(0, 1)
+
+        # apply final epsilon prediction layer
+        return self.eps_out(ac_tokens)
 
 
-class DiffusionMLPAgent(Agent):
+class DiffusionTransformerAgent(Agent):
     def __init__(self, features, shared_mlp, odim, n_cams, use_obs, 
                  ac_dim, ac_chunk, train_diffusion_steps, eval_diffusion_steps,
                  imgs_per_cam=1, dropout=0, share_cam_features=False, 
@@ -107,8 +135,8 @@ class DiffusionMLPAgent(Agent):
         super().__init__(features, None, shared_mlp, odim, n_cams, 
                          use_obs, imgs_per_cam, dropout, share_cam_features,
                          feat_batch_norm)
-        self.noise_net = NoiseNetwork(adim=ac_dim, ac_chunk=ac_chunk, 
-                                      cond_dim=self.obs_enc_dim,
+        self.noise_net = NoiseNetwork(ac_dim=ac_dim, ac_chunk=ac_chunk,
+                                      obs_dim=self.obs_enc_dim,
                                       **noise_net_kwargs)
         self._ac_dim, self._ac_chunk = ac_dim, ac_chunk
         
@@ -126,18 +154,22 @@ class DiffusionMLPAgent(Agent):
                                     prediction_type="epsilon"
                                     )
         
-    def forward(self, imgs, obs, actions, mask):
+    def forward(self, imgs, obs, ac_flat, mask_flat):
         # get observation encoding and sample noise/timesteps
         B, device = obs.shape[0], obs.device
         s_t = self._shared_forward(imgs, obs)
         timesteps = torch.randint(low=0, high=self._train_diffusion_steps, size=(B,), 
                                   device=device).long()
+
+        # diffusion unet logic assumes [B, T, adim]
+        mask = mask_flat.reshape((B, self.ac_chunk, self.ac_dim))
+        actions = ac_flat.reshape((B, self.ac_chunk, self.ac_dim))
         noise = torch.randn_like(actions)
 
         # construct noise actions given real actions, noise, and diffusion schedule
         noise_acs = self.diffusion_schedule.add_noise(actions, noise, timesteps)
         noise_pred = self.noise_net(noise_acs, timesteps, s_t)
-        
+
         # calculate loss for noise net
         loss = nn.functional.mse_loss(noise_pred, noise, reduction="none")
         loss = (loss * mask).sum(1)    # mask the loss to only consider "real" acs
@@ -147,7 +179,7 @@ class DiffusionMLPAgent(Agent):
         # get observation encoding and sample noise
         B, device = obs.shape[0], obs.device
         s_t = self._shared_forward(imgs, obs)
-        noise_actions = torch.randn(B, self.ac_chunk * self.ac_dim, device=device)
+        noise_actions = torch.randn(B, self.ac_chunk, self.ac_dim, device=device)
 
         # set number of steps
         eval_steps = self._eval_diffusion_steps
@@ -170,7 +202,7 @@ class DiffusionMLPAgent(Agent):
                                                 sample=noise_actions).prev_sample
 
         # return final action post diffusion
-        return noise_actions.reshape((B, self.ac_chunk, self.ac_dim))
+        return noise_actions
     
     @property
     def ac_chunk(self):
