03a6f0fc5f1f4ad0eb8608fbe05fd388680bea02
Sudeep Dasari <sdasari@andrew.cmu.edu>
2024-09-22 04:51:36 +0000

    initial refactors

--- data4robotics/replay_buffer.py
+++ data4robotics/replay_buffer.py
@@ -16,15 +16,16 @@ from robobuf import ReplayBuffer as RB
 from tensorflow.io import gfile
 from torch.utils.data import Dataset, IterableDataset
 
+# cache loading from the buffer list to half memory overhead
+buf_cache = dict()
+BUF_SHUFFLE_RNG = 3904767649
+
+
 # helper functions
 _img_to_tensor = (
     lambda x: torch.from_numpy(x.copy()).permute((0, 3, 1, 2)).float() / 255
 )
 _to_tensor = lambda x: torch.from_numpy(x).float()
-
-
-# cache loading from the buffer list to half memory overhead
-buf_cache = dict()
 
 
 def _cached_load(path):
@@ -49,70 +50,6 @@ def _get_imgs(t, cam_idx, past_frames):
     return np.concatenate(imgs, axis=0)
 
 
-BUF_SHUFFLE_RNG = 3904767649
-
-
-class ReplayBuffer(Dataset):
-    def __init__(
-        self, buffer_path, transform=None, n_train_demos=200, mode="train", ac_chunk=1
-    ):
-        assert mode in ("train", "test"), "Mode must be train/test"
-        buffer_data = self._load_buffer(buffer_path)
-        assert len(buffer_data) >= n_train_demos, "Not enough demos!"
-
-        # shuffle the list with the fixed seed
-        rng = random.Random(BUF_SHUFFLE_RNG)
-        rng.shuffle(buffer_data)
-
-        # split data according to mode
-        buffer_data = (
-            buffer_data[:n_train_demos]
-            if mode == "train"
-            else buffer_data[n_train_demos:]
-        )
-
-        self.transform = transform
-        self.s_a_mask = []
-        for traj in tqdm.tqdm(buffer_data):
-            imgs, obs, acs = traj["images"], traj["observations"], traj["actions"]
-            assert len(obs) == len(acs) and len(acs) == len(
-                imgs
-            ), "All time dimensions must match!"
-
-            # pad camera dimension if needed
-            if len(imgs.shape) == 4:
-                imgs = imgs[:, None]
-
-            for t in range(len(imgs) - ac_chunk):
-                i_t = {f"cam{c}": imgs[t, c] for c in range(imgs.shape[1])}
-                loss_mask = np.ones((ac_chunk,), dtype=np.float32)
-                o_t, a_t = obs[t], acs[t : t + ac_chunk]
-                self.s_a_mask.append(((i_t, o_t), a_t, loss_mask))
-
-    def _load_buffer(self, buffer_path):
-        print("loading", buffer_path)
-        with open(buffer_path, "rb") as f:
-            buffer_data = pkl.load(f)
-        return buffer_data
-
-    def __len__(self):
-        return len(self.s_a_mask)
-
-    def __getitem__(self, idx):
-        (i_t, o_t), a_t, loss_mask = self.s_a_mask[idx]
-
-        i_t = {k: _img_to_tensor(v) for k, v in i_t.items()}
-        if self.transform is not None:
-            i_t = {k: self.transform(v) for k, v in i_t.items()}
-
-        o_t, a_t = _to_tensor(o_t), _to_tensor(a_t)
-        loss_mask = _to_tensor(loss_mask)[:, None].repeat((1, a_t.shape[-1]))
-        assert (
-            loss_mask.shape[0] == a_t.shape[0]
-        ), "a_t and mask shape must be ac_chunk!"
-        return (i_t, o_t), a_t, loss_mask
-
-
 class IterableWrapper(IterableDataset):
     def __init__(self, wrapped_dataset, max_count=float("inf")):
         self.wrapped = wrapped_dataset
@@ -131,7 +68,7 @@ class IterableWrapper(IterableDataset):
         return self.wrapped[idx]
 
 
-class RobobufReplayBuffer(ReplayBuffer):
+class RobobufReplayBuffer(Dataset):
     def __init__(
         self,
         buffer_path,
@@ -198,6 +135,9 @@ class RobobufReplayBuffer(ReplayBuffer):
 
             loss_mask = np.array(loss_mask, dtype=np.float32)
             self.s_a_mask.append((t, a_t, loss_mask, loop_t))
+
+    def __len__(self):
+        return len(self.s_a_mask)
 
     def __getitem__(self, idx):
         step, a_t, loss_mask, goal = self.s_a_mask[idx]
